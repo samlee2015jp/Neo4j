e5d9954cfbd1ef2a3d6bd2278e88c65d8848e7ca
rahmanmdmizanur <rahman.mdmizanur@aist.go.jp>
2020-10-23 18:45:30 +0900

    first commit

---
+++ create_nodes.py
@@ -0,0 +1,20 @@
+from neo4j import GraphDatabase
+
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "password"))
+
+
+def create_friend_of(tx, name, friend):
+    tx.run("CREATE (a:Person)-[:KNOWS]->(f:Person {name: $friend})"
+           "WHERE a.name = $name "
+           "RETURN f.name AS friend", name=name, friend=friend
+           )
+
+
+with driver.session() as session:
+    session.write_transaction(create_friend_of, "Alice", "Bob")
+
+with driver.session as session:
+    session.write_transaction(create_friend_of, "Alice", "Carl")
+
+driver.close()
