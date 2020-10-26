a82b1b4729fbe40a4e10fddf58bf57958b0fe059
rahmanmdmizanur <rahman.mdmizanur@aist.go.jp>
2020-10-26 18:59:14 +0900

    add nlp

--- corenlp_python/use_corenlp.py
+++ corenlp_python/use_corenlp.py
@@ -7,12 +7,13 @@ from stanfordcorenlp import StanfordCoreNLP
 
 nlp = StanfordCoreNLP(
     r'D:\samli_202010\CoreNLP\CoreNLP\stanford-corenlp-4.1.0')
+# nlp = StanfordCoreNLP('http://localhost', port=9000)
 
-sentence = 'Guangdong University of Foreign Studies is located in Guangzhou.'
+sentence = 'I go to aist in Tokyo everyday.Tokyo is the capital city of Japan.'
 print('Tokenize:', nlp.word_tokenize(sentence))
 print('Part of Speech:', nlp.pos_tag(sentence))
 print('Named Entities:', nlp.ner(sentence))
-print('Constituency Parsing:', nlp.parse(sentence))
+# print('Constituency Parsing:', nlp.parse(sentence))
 print('Dependency Parsing:', nlp.dependency_parse(sentence))
 
 # Do not forget to close! The backend server will consume a lot memery.
