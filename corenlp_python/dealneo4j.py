3aa71d1f596d19888e836d6fda378361d7efbcf7
rahmanmdmizanur <rahman.mdmizanur@aist.go.jp>
2020-10-27 18:22:49 +0900

    add relation

--- corenlp_python/dealneo4j.py
+++ corenlp_python/dealneo4j.py
@@ -16,18 +16,31 @@ class DealNeo4j:
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
-    def create_relationship(self, person1_name, person2_name, relation_ship):
+    def create_relationship(self, person1_name, person2_name, relation):
         with self.driver.session() as session:
+            # entity1
+            entityResult1 = self._find_entity(self, person1_name)
+            print("entityResult1: ", entityResult1)
+
+            # entity2
+            entityResult2 = self._find_entity(self, person2_name)
+            print("entityResult2: ", entityResult2)
+
+            # entity2
+            # relationResult = self._find_relation(
+            #     self, person1_name, person2_name, relation)
+            # print("relationResult: ", relationResult)
+
             # Write transactions allow the driver to handle retries and transient errors
             result = session.write_transaction(
-                self._create_and_return_relationship, person1_name, person2_name, relation_ship)
+                self._create_and_return_relationship, person1_name, person2_name, relation)
             for record in result:
-                print("Created friendship between: {p1}, {p2}".format(
-                    p1=record['p1'], p2=record['p2']
+                print("Created friendship between: {p1}, {p2}, {r}".format(
+                    p1=record['p1'], p2=record['p2'], r=record['r']
                 ))
 
     @staticmethod
-    def _create_and_return_relationship(tx, person1_name, person2_name, relation_ship):
+    def _create_and_return_relationship(tx, person1_name, person2_name, relation):
 
         # To learn more about the Cypher syntax.
         # see http://neo4j.com/docs/cypher-manual/current/
@@ -38,14 +51,16 @@ class DealNeo4j:
         query = (
             "CREATE (p1:Entity { name: $person1_name }) "
             "CREATE (p2:Entity { name: $person2_name }) "
-            "CREATE (p1)-[r:" + relation_ship + "]->(p2) "
-            "RETURN p1, p2"
+            "CREATE (p1)-[r:" + relation + "]->(p2) "
+            "RETURN p1, p2, r"
         )
         result = tx.run(query, person1_name=person1_name,
-                        person2_name=person2_name, relation_ship=relation_ship)
+                        person2_name=person2_name, relation=relation)
         try:
             return [{"p1": record["p1"]["name"],
-                     "p2": record["p2"]["name"]}
+                     "p2": record["p2"]["name"],
+                     "r":record["r"]
+                     }
                     for record in result]
         # Capture any errors along with the query and data for traceability
         except ServiceUnavailable as exception:
@@ -53,21 +68,43 @@ class DealNeo4j:
                 query=query, exception=exception))
             raise
 
-    def find_person(self, person_name):
+    @staticmethod
+    def _find_entity(self, person_name):
         with self.driver.session() as session:
             result = session.read_transaction(
-                self._find_and_return_person, person_name)
+                self._find_and_return_entity, person_name)
             for record in result:
-                print("Found person: {record}".format(record=record))
+                print("Found entity: {record}".format(record=record))
+                return record
 
     @staticmethod
-    def _find_and_return_person(tx, person_name):
+    def _find_and_return_entity(tx, person_name):
         query = (
             "MATCH (p:Entity) "
             "WHERE p.name = $person_name "
             "RETURN p.name AS name"
         )
         result = tx.run(query, person_name=person_name)
+        return [record["name"] for record in result]
+
+    @staticmethod
+    def _find_relation(self, person1_name, person2_name, relation):
+        with self.driver.session() as session:
+            result = session.read_transaction(
+                self._find_and_return_entity, person1_name=person1_name, person2_name=person2_name, relation=relation)
+            for record in result:
+                print("Found entity: {record}".format(record=record))
+                return record
+
+    @staticmethod
+    def _find_and_return_relation(tx, person1_name, person2_name, relation):
+        query = (
+            "MATCH (p:Entity)-[r:" + relation + "]-(s:Entity)"
+            "WHERE p.name = $person1_name and s.name = $person2_name"
+            "RETURN r AS relation"
+        )
+        result = tx.run(query, person1_name=person1_name,
+                        person2_name=person2_name, relation=relation)
         return [record["name"] for record in result]
 
 
@@ -86,8 +123,8 @@ if __name__ == "__main__":
     # the sentence
     sentence = 'I go to aist in Tokyo everyday.'
     nlp = NLP(sentence)
-    print("ner: ", nlp.get_ner())
-    print("dependency_parse: ", nlp.get_dependency_parse())
+    # print("ner: ", nlp.get_ner())
+    # print("dependency_parse: ", nlp.get_dependency_parse())
     ner = nlp.get_ner()
     dependencies = nlp.get_dependency_parse()
     for dependency in dependencies:
@@ -96,13 +133,18 @@ if __name__ == "__main__":
         entity2 = ''
         if dependency[1] != 0:
             entity1 = ner[dependency[1]-1][0]
+            # find entity
+            # print("entity1: ", entity1)
+            # myneo4j.find_entity(entity1)
 
         if dependency[2] != 0:
             entity2 = ner[dependency[2]-1][0]
+            # find entity
+            # print("entity2: ", entity2)
+            # myneo4j.find_entity(entity2)
 
         # create relationship
         myneo4j.create_relationship(
             entity1, entity2, dependency[0].replace(':', ''))
 
-    # app.find_person("Alice")
     myneo4j.close()
