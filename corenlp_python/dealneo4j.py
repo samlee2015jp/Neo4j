3b3499ae6d4f7e6d8bf0646bafa6bf7429ef3773
rahmanmdmizanur <rahman.mdmizanur@aist.go.jp>
2020-10-28 18:30:13 +0900

    add

--- corenlp_python/dealneo4j.py
+++ corenlp_python/dealneo4j.py
@@ -18,47 +18,50 @@ class DealNeo4j:
 
     def create_relation(self, property1_name, property2_name, relation):
         with self.driver.session() as session:
-            # entity1
-            propertyResult1 = None
+            # property name and relation
+            print("property1_name: ", property1_name)
+            print("property2_name: ", property2_name)
+            print("relation: ", relation)
 
+            # propertyResult1
+            propertyResult1 = None
             if property1_name != '':
-                propertyResult1 = self._find_entity_property(
+                propertyResult1 = self._find_property(
                     self, property1_name)
-                print("entityResult1: ", propertyResult1)
             else:
                 propertyResult1 = ''
+            print("propertyResult1: ", propertyResult1)
 
-            # entity2
+            # propertyResult2
             propertyResult2 = None
-
             if property2_name != '':
-                propertyResult2 = self._find_entity_property(
+                propertyResult2 = self._find_property(
                     self, property2_name)
-                print("propertyResult2: ", propertyResult2)
-                if propertyResult2 == None:
-                    print("propertyResult2: ", propertyResult2)
 
             else:
                 propertyResult2 = ''
+            print("propertyResult2: ", propertyResult2)
 
-            # relation
-            if relation == 'ROOT':
-                relation = None
+            # relationResult
             relationResult = None
+            if ((relation != 'ROOT') and (relation != '')):
+                relationResult = self._find_relation(
+                    self, relation)
+            else:
+                relationResult = ''
+            print("relationResult: ", relationResult)
 
             # Write transactions allow the driver to handle retries and transient errors
-            if (relationResult == None) and (propertyResult1 == None) and (propertyResult2 != None):
-                result = session.write_transaction(
-                    self._create_and_return_relation_entity, property1_name, property2_name, relation)
-                for record in result:
-                    print("Created relation between: {p1}, {p2}, {r}".format(
-                        p1=record['p1'], p2=record['p2'], r=record['r']
-                    ))
+            if (propertyResult1 != None) and (propertyResult2 == None) and (relationResult != None):
+                createPropertyResult2 = session.write_transaction(
+                    self._create_and_return_property, property2_name)
+                print('created property2: ' + createPropertyResult2)
 
             # when it doesnot exist entity1
+            print('The end of creating each property or relation.')
 
     @staticmethod
-    def _create_and_return_property(tx, entity1_name, entity2_name, relation):
+    def _create_and_return_property(tx, property_name):
 
         # To learn more about the Cypher syntax.
         # see http://neo4j.com/docs/cypher-manual/current/
@@ -67,34 +70,23 @@ class DealNeo4j:
         # see https://neo4j.com/docs/cypher-refcard/current/
 
         query = (
-            "CREATE (p1)-[r:" + relation + "]->(p2) "
-            "RETURN p1, p2, r"
+            "CREATE (e:Entity {name: $property_name}) "
+            "RETURN e.name AS name"
         )
-        result = tx.run(query, entity1_name=entity1_name,
-                        entity2_name=entity2_name, relation=relation)
-        try:
-            return [{"p1": record["p1"]["name"],
-                     "p2": record["p2"]["name"],
-                     "r":record["r"]
-                     }
-                    for record in result]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
+        result = tx.run(query, property_name=property_name)
+        return [record["name"] for record in result]
 
     @staticmethod
-    def _find_entity_property(self, property_name):
+    def _find_property(self, property_name):
         with self.driver.session() as session:
             result = session.read_transaction(
-                self._find_and_return_entity_property, property_name)
+                self._find_and_return_property, property_name)
             for record in result:
                 print("Found property name: {record}".format(record=record))
                 return record
 
     @staticmethod
-    def _find_and_return_entity_property(tx, property_name):
+    def _find_and_return_property(tx, property_name):
         query = (
             "MATCH (p:Entity) "
             "WHERE p.name = $property_name "
@@ -102,6 +94,24 @@ class DealNeo4j:
         )
         result = tx.run(query, property_name=property_name)
         return [record["name"] for record in result]
+
+    @staticmethod
+    def _find_relation(self, relation):
+        with self.driver.session() as session:
+            result = session.read_transaction(
+                self._find_and_return_property, relation)
+            for record in result:
+                print("Found property name: {record}".format(record=record))
+                return record
+
+    @staticmethod
+    def _find_and_return_relation(tx, relation):
+        query = (
+            "MATCH (n)-[r:$relation]->(m) "
+            "RETURN type(r) AS relation"
+        )
+        result = tx.run(query, relation=relation)
+        return [record["relation"] for record in result]
 
 
 if __name__ == "__main__":
