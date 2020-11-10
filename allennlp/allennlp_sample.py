01af1d1e5e855abbb22148e365c19d6fb2a83689
unknown <ri.sam2020@aist.go.jp>
2020-11-10 15:53:31 +0900

    add

---
+++ allennlp/allennlp_sample.py
@@ -0,0 +1,39 @@
+from typing import Dict, Iterable, List
+
+from allennlp.data import DatasetReader, Instance
+from allennlp.data.fields import LabelField, TextField
+from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
+from allennlp.data.tokenizers import Token, Tokenizer, WhitespaceTokenizer
+
+
+class ClassificationTsvReader(DatasetReader):
+    def __init__(self,
+                 lazy: bool = False,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 max_tokens: int = None):
+        super().__init__(lazy)
+        self.tokenizer = tokenizer or WhitespaceTokenizer()
+        self.token_indexers = token_indexers or {
+            'tokens': SingleIdTokenIndexer()}
+        self.max_tokens = max_tokens
+
+    def _read(self, file_path: str) -> Iterable[Instance]:
+        with open(file_path, 'r') as lines:
+            for line in lines:
+                text, sentiment = line.strip().split('\t')
+                tokens = self.tokenizer.tokenize(text)
+                if self.max_tokens:
+                    tokens = tokens[:self.max_tokens]
+                text_field = TextField(tokens, self.token_indexers)
+                label_field = LabelField(sentiment)
+                fields = {'text': text_field, 'label': label_field}
+                yield Instance(fields)
+
+
+dataset_reader = ClassificationTsvReader(max_tokens=64)
+instances = dataset_reader.read(
+    r"D:\samli_202010\task\Neo4j\allennlp\data\cv000_29416.txt")
+
+for instance in instances[:10]:
+    print(instance)
